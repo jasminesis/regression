9408bdb35cecb19ca7fc175ebc7ab86ca40d9294
Jasmine S <31816654+jasminesis@users.noreply.github.com>
2023-12-04 23:35:24 -0500

    logistic function works

--- logistic.R
+++ logistic.R
@@ -1,63 +1,38 @@
-require(tidyverse)
+library(tidyverse)
+library(alr4)
+set.seed(1)
 
-logit <- qlogis
 invlogit <- plogis
-
-dgp <- function() {
-  n <- 200
-  x <- sample(c(seq(from = 0, to = 6, by = 0.05)), n, replace = T)
-  y <- rbinom(n, 1, x / 6)
-  tibble(y, x) %>% arrange(x)
+optim_logistic <- function(beta) {
+  alpha <- beta[1]
+  b1 <- beta[2]
+  b2 <- beta[3]
+  b3 <- beta[4]
+  pi <- invlogit(alpha + b1 * data$x1 + b2 * data$x2 + b3 * data$x3)
+  loglikelihood <- -sum(data$y * log(pi) + (1 - data$y) * log(1 - pi))
+  return(loglikelihood)
 }
-data <- dgp()
+data <- data.frame(x1 = rnorm(100, 2, 1), x2 = rnorm(100, 4, 1), x3 = rnorm(100, 6, 1))
+data$y <- rbinom(100, size = 1, prob = invlogit(-1 + data$x1 + data$x2 - 0.5 * data$x3))
 
-fit <- glm(y ~ x, family = binomial(link = "logit"), data = data)
-fit
-ggplot(data) +
-  geom_point(aes(x = x, y = y)) +
-  geom_smooth(aes(x = x, y = y), method = "glm", method.args = list(family = "binomial"), se = F)
+result <- optim(par = rep(0, ncol(data)), fn = optim_logistic)
+result$par
 
-# taken from https://francish.net/post/logistic-regression-fisher-scoring/
-
-### extracting raw components
-
-dat <- model.frame(fit)
-fml <- formula(fit)
-X <- model.matrix(fml, dat)
-y <- model.response(dat, "numeric")
-k <- ncol(X)
-# pp <- function(x) 1 / (1 + exp(-x)) #convert logit to pred prob
-
-beta_0 <- rep(0, k) # initialize with zeroes
-eta <- X %*% beta_0 # predicted logit
-mu <- plogis(eta) # same as pp but built-in; convert logit to pred prob
-vr <- (mu * (1 - mu)) # variance
-
-wts <- diag(as.vector(vr)) # create weight matrix
-d2 <- t(X) %*% wts %*% X # the Fisher information matrix
-u1 <- t(X) %*% (y - mu) # the score function
-iter <- 50 # max number of iterations
-tol <- 1e-8 # tolerance
-
-### now iterate
-
-for (i in 1:iter) {
-  # beta_1 <- beta_0 + solve(d2) %*% u1 #update beta
-  beta_1 <- beta_0 + chol2inv(chol(d2)) %*% u1 # update beta avoiding inversion
-  print(beta_1) #show estimates
-
-  if (any(abs((beta_1 - beta_0) / beta_0) < tol)) break # stop loop if no change
-
-  eta <- X %*% beta_1 # predicted logit
-  mu <- plogis(eta) # this is just pp(eta)
-  vr <- mu * (1 - mu) # variance
-
-  wts <- diag(as.vector(vr)) # putting in a weight matrix
-  d2 <- t(X) %*% wts %*% X # information matrix
-  u1 <- t(X) %*% (y - mu) # score function
-  beta_0 <- beta_1
-  cat(i, "::") # show iteration number
-}
+glm(y ~ x1 + x2 + x3, data = data, family = binomial)
 
 
+# extend the function to be able to take in categorical data
+Donner <- Donner %>% mutate(survived = y == "survived")
+m1 <- glm(survived ~ age + sex + status, family = binomial(link = "logit"), data = Donner)
+ggplot(Donner) +
+  geom_jitter(aes(x = status, y = age, col = survived))
 
+ggplot(Donner) +
+  geom_jitter(aes(x = age, y = survived, color = survived)) +
+  facet_wrap(vars(status))
+ggplot(Donner) +
+  geom_jitter(aes(x = age, y = status, color = status)) +
+  facet_wrap(vars(survived))
+
+fit <- glm(survived ~ age + sex + status, data = Donner, family = "binomial")
+coef(fit)
