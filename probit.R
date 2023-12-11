767a3728bec66fda03596be0f652a7d63db8a9e8
Saumya Seth <saumyaseth256@gmail.com>
2023-12-11 15:14:58 -0500

    probit update with se

--- probit.R
+++ probit.R
@@ -14,18 +14,43 @@ probit_regression <- function(data, x1, x2, ..., y) {
     return(loglikelihood)
   }
   initial_guess <- matrix(0, nrow = ncol(data), ncol = 1)
-  result <-
-    optim(
-      initial_guess,
+  result <- optim(
+    initial_guess,
+    probit.loglikelihood,
+    X = X,
+    Y = Y,
+    method = 'BFGS'
+  )
+  estimate <-
+    c(result$par[1], result$par[2], result$par[3], result$par[4])
+  num_bootstraps <- 100
+  result_bootstrap <-
+    matrix(0, nrow = num_bootstraps, ncol = length(estimate))
+  for (i in 1:num_bootstraps) {
+    sample_indices <- sample(nrow(data), replace = TRUE)
+    bootstrap_data <- data[sample_indices,]
+    X_bootstrap <-
+      matrix(
+        c(rep(1, nrow(bootstrap_data)), x_parameters),
+        nrow = nrow(bootstrap_data),
+        ncol = ncol(bootstrap_data)
+      )
+    Y_bootstrap <-
+      matrix(bootstrap_data$y,
+             nrow = nrow(bootstrap_data),
+             ncol = 1)
+    initial_guess_bootstrap <-
+      matrix(0, nrow = ncol(bootstrap_data), ncol = 1)
+    result_bootstrap[i, ] <- optim(
+      initial_guess_bootstrap,
       probit.loglikelihood,
-      X = X,
-      Y = Y,
+      X = X_bootstrap,
+      Y = Y_bootstrap,
       method = 'BFGS'
-    )
-  c(result$par[1],
-    result$par[2],
-    result$par[3],
-    result$par[4])
+    )$par
+  }
+  se <- apply(result_bootstrap, 2, sd)
+  return(list(estimate, se))
 }
 # test data frame for probit regression
 test_probit_regression_data <- data.frame(
@@ -41,11 +66,18 @@ our_implementation_probit <- probit_regression(
   x2 = test_probit_regression_data$x2,
   x3 = test_probit_regression_data$x3,
   y = test_probit_regression_data$y
-)[1:4]
+)
 # comparing results to glm probit output
-r_implementation_probit <- glm(y ~ x1 + x2 + x3,
-                               data = test_probit_regression_data,
-                               family = binomial(link = "probit"))$coefficients[1:4]
+r_implementation_probit <- summary(glm(
+  y ~ x1 + x2 + x3,
+  data = test_probit_regression_data,
+  family = binomial(link = 'probit')
+))$coefficients[, 1:2]
 
+our_implementation_probit_estimates <- unlist(our_implementation_probit)[1:4]
+our_implementation_probit_se <- unlist(our_implementation_probit)[5:8]
+our_implementation_probit <- data.frame(our_implementation_probit_estimates, our_implementation_probit_se)
+row.names(our_implementation_probit) <- c('(Intercept)', 'x1', 'x2', 'x3')
+colnames(our_implementation_probit) <- c('Estimate', 'Std. Error')
 our_implementation_probit
 r_implementation_probit
